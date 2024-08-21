4e5d41485fe5adbbbba6cab3beb151e1b547fb1d
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-08-21 22:35:22 +0530

    added proxy-server

---
+++ reverse-proxy/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import httpProxy from "http-proxy";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+const BASE_PATH =
+  "https://graphql-eu-north-1.s3.eu-north-1.amazonaws.com/__outputs/";
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+  const subdomain = req.hostname.split(".")[0];
+  const resolvesTo = `${BASE_PATH}${subdomain}`;
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+app.listen(8080, () => {
+  console.log("Server is running on port " + PORT);
+});
